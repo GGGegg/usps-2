8a4f070e216156d1fdfff1aeda6b3fa8f0267655
GGGegg <582578359@qq.com>
2017-12-14 16:27:47 -0500

    add count and log distance method

--- src/generGshortestpath.py
+++ src/generGshortestpath.py
@@ -31,6 +31,17 @@ def GenerWeightedG(data3):
     for i in data3.index:
         Gcity.add_edge( data3['departure'][i],data3['destination'][i],weight=data3['weighted_distance'][i])
     return Gcity
+def GenerCountG(data3):
+    Gcity=nx.Graph()
+    for i in data3.index:
+        Gcity.add_edge( data3['departure'][i],data3['destination'][i],weight=data3[1][i])
+    return Gcity
+
+def GenerLogG(data3):
+    Gcity=nx.Graph()
+    for i in data3.index:
+        Gcity.add_edge( data3['departure'][i],data3['destination'][i],weight=data3['log'][i])
+    return Gcity
 
 def zipcodeToLL(zipcodes):
     ziptoloc_pd = pd.read_csv("../data/zipToLngLat.txt")
@@ -43,29 +54,38 @@ def zipcodeToLL(zipcodes):
             ll.append(ziptoloc[i])
     return ll
 
-def drawGraph(true,sp,wsp):
+def drawGraph(true,sp,wsp,cp,lp):
     import gmplot
     import numpy as np
     truth_path = np.array(zipcodeToLL(true))
     sp_path = np.array(zipcodeToLL(sp))
+    cp_path = np.array(zipcodeToLL(cp))
+    lp_path = np.array(zipcodeToLL(lp))
     wsp_path = np.array(zipcodeToLL(wsp))
     gmap = gmplot.GoogleMapPlotter(37.428, -122.145, 16)
 
     gmap.plot(truth_path[:,0], truth_path[:,1], 'cornflowerblue', edge_width=5)
     gmap.plot(sp_path[:,0], sp_path[:,1], 'green', edge_width=1)
     gmap.plot(wsp_path[:,0], wsp_path[:,1], 'red', edge_width=1)
+    gmap.plot(wsp_path[:, 0], cp_path[:, 1], 'black', edge_width=1)
+    gmap.plot(wsp_path[:, 0], lp_path[:, 1], 'grey', edge_width=1)
     gmap.draw("../out/compare.html")
 
 # data3=pd.read_csv("../out/graph.csv" )
 G=GenerG(data3)
 WG = GenerWeightedG(data3)
+CG = GenerCountG(data3)
+LG = GenerLogG(data3)
 result = []
 weighted_result = []
-log_result = []
+cg_result = []
+lg_result = []
 valid_trips = []
 sp = []
 wsp = []
 lsp = []
+cp= []
+lp = []
 for i in range(len(test)):
     t = test[i]
     if len(t)>1:
@@ -73,21 +93,35 @@ for i in range(len(test)):
             try:
                 Shortest_path =nx.dijkstra_path(G,t[0],t[-1])
                 sp.append(Shortest_path)
-                # length =nx.dijkstra_path_length(G,t[0],t[-1])
                 s = score(t,Shortest_path)
                 result.append(s)
+
                 shortest_path= nx.dijkstra_path(WG,t[0],t[-1])
                 wsp.append(shortest_path)
                 wScore = score(t, shortest_path)
                 weighted_result.append(wScore)
+
+                Csp = nx.dijkstra_path(CG,t[0],t[-1])
+                cp.append(Csp)
+                cScore = score(t,Csp)
+                cg_result.append(cScore)
+
+                Lsp = nx.dijkstra_path(LG,t[0],t[-1])
+                lp.append(Lsp)
+                lScore = score(t,Lsp)
+                lg_result.append(lScore)
+
+
                 valid_trips.append(t)
             except :
                 continue
 # its for compare ground truth with our two approach
 # it will generate one google map with three links
 # try more and take 5 good result and 5 bad result for the report
-# drawGraph(valid_trips[200],sp[200],wsp[200])
-result = np.array(result)
-weighted_result = np.array(weighted_result)
-print(result.mean())
-print(weighted_result.mean())
+drawGraph(valid_trips[200],sp[200],wsp[200],cp[200],lp[200])
+# result = np.array(result)
+# weighted_result = np.array(weighted_result)
+# print(result.mean())
+# print(weighted_result.mean())
+cg_result = np.array(cg_result)
+print(cg_result.mean())
